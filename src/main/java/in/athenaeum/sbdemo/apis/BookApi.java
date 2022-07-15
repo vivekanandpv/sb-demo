ec355594d7bb4f8abef00a05875e93715e36aa7d
Vivekanand P V <vivekanandpv@outlook.com>
2022-07-15 12:50:19 +0530

    returning response-entity

--- src/main/java/in/athenaeum/sbdemo/apis/BookApi.java
+++ src/main/java/in/athenaeum/sbdemo/apis/BookApi.java
@@ -1,38 +1,41 @@
 package in.athenaeum.sbdemo.apis;
 
 import in.athenaeum.sbdemo.models.Book;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/books")
 public class BookApi {
     @GetMapping
-    public Book getBook(@RequestParam(required = false) String title) {
-        return new Book(0, title, "10th International", 2021, 1200);
+    public ResponseEntity<Book> getBook(@RequestParam(required = false) String title) {
+        return ResponseEntity.ok(new Book(0, title, "10th International", 2021, 1200));
     }
 
     @GetMapping("{id}")
-    public Book getBookById(@PathVariable int id) {
-        return new Book(id, "Learning Java", "10th International", 2020, 1200);
+    public ResponseEntity<Book> getBookById(@PathVariable int id) {
+        return ResponseEntity.ok(new Book(id, "Learning Java", "10th International", 2020, 1200));
     }
 
     @PostMapping
-    public Book createBook(@RequestBody Book book) {
-        return book;
+    public ResponseEntity<Book> createBook(@RequestBody Book book) {
+        return ResponseEntity.ok(book);
     }
 
     @PutMapping("{id}")
-    public Book updateBook(@RequestBody Book book, @PathVariable int id) {
-        return book;
+    public ResponseEntity<Book> updateBook(@RequestBody Book book, @PathVariable int id) {
+        return ResponseEntity.ok(book);
     }
 
     @PatchMapping("{id}")
-    public Book updateBook1(@RequestBody Book book, @PathVariable int id) {
-        return book;
+    public ResponseEntity<Book> updateBook1(@RequestBody Book book, @PathVariable int id) {
+        return ResponseEntity.ok(book);
     }
 
     @DeleteMapping("{id}")
-    public void deleteById(@PathVariable int id) {
-
+    public ResponseEntity<?> deleteById(@PathVariable int id) {
+        return ResponseEntity.noContent().build();
+//        return ResponseEntity.ok().build();
+//        return ResponseEntity.status(204).build();
     }
 }
